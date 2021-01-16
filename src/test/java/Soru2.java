95d44d2bdd59e508d8afc87d1f1f64430b0d85f5
husamettinszn <husamettinszn@gmail.com>
2021-01-16 18:02:19 +0100

    sout

---
+++ src/test/java/Soru2.java
@@ -0,0 +1,7 @@
+public class Soru2 {
+    public static void main(String[] args) {
+        System.out.println("Merheba Dunya");
+        System.out.printf("Hello World");
+        System.out.println("Hallo Welt");
+    }
+}
