0e83bc54b3ec59d2d7b4da0ba11175e8d7afb32a
husamettinszn <husamettinszn@gmail.com>
2021-01-19 17:35:21 +0100

    string

--- src/test/java/Soru3.java
+++ src/test/java/Soru3.java
@@ -3,38 +3,13 @@ import java.util.List;
 
 public class Soru3 {
     public static void main(String[] args) {
-             /*
-    int arr[] = { 3, 5, 1, 2, 7, 9, 2, 3, 5, 7};
-    Verilen arrayi kullanarak asagidaki gibi bir list return eden bir method olusturun
-    [arr'nin ilk elemani, arr'nin 2. ve 3. elemanlari toplami,
-    arr'nin 4. 5. ve 6. elemanlari toplami, arr'nin 7. 8. 9. ve 10. elemanlari toplami ... ]
-   */
-        int arr[] = { 3, 5, 1, 2, 7, 9, 2, 3, 5, 7};
-        add(arr);
+        //String'den benzersiz deger nasil yazdirilir?
 
-    }
-    public static void add(int arr[]){
+        String  str = "tteetccehchppteerrocopt";
 
-        List<Integer> list = new ArrayList<Integer>();
-
-        int i = 0;
-
-        while (i<arr.length){
-
-            int bas = i*(i+1)/2;
-            int bit = (i+1)*(i+2)/2;
-
-            if(bit>arr.length){
-                break;
-            }
-            int sum= 0;
-            for(; bas<bit; bas++){
-                sum = sum + arr[bas];
-            }
-            list.add(sum);
-            i++;
+        String unique = "";
+        for(int i=0; i<str.length(); i++){
+            if
         }
-        System.out.println(list);
     }
-
 }
