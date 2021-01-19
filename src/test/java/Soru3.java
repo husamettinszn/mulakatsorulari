f24c582dc222045646204b79a77bcbedcf977fd3
husamettinszn <husamettinszn@gmail.com>
2021-01-19 22:37:53 +0100

    soru

--- src/test/java/Soru3.java
+++ src/test/java/Soru3.java
@@ -1,5 +1,5 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.awt.*;
+import java.util.*;
 
 public class Soru3 {
     public static void main(String[] args) {
@@ -7,9 +7,53 @@ public class Soru3 {
 
         String  str = "tteetccehchppteerrocopt";
 
+
         String unique = "";
         for(int i=0; i<str.length(); i++){
-            if()
+            if(!unique.contains(str.charAt(i)+"")){
+                unique= unique + str.charAt(i);
+            }
         }
-    }
+        System.out.println(unique);
+
+
+        System.out.println("===============================");
+
+        String  arr[] = str.split("");
+
+        String tekrarsiz= "";
+        for (String w: arr
+             ) {
+            if(!tekrarsiz.contains(w)){
+                tekrarsiz= tekrarsiz + w;
+            }
+        }
+        System.out.println(tekrarsiz);
+
+        System.out.println("=========================");
+
+        Set<Character> setC = new HashSet<>();
+
+        for (int k=0; k<str.length(); k++){
+            setC.add(str.charAt(k));
+        }
+        System.out.println(setC);
+
+
+        System.out.println("=========================");
+
+        LinkedHashSet<Character> lhset = new LinkedHashSet<>();
+        for (int k=0; k<str.length(); k++){
+            lhset.add(str.charAt(k));
+        }
+        System.out.println(lhset);
+
+        String noTekrar="";
+
+        for (Character w: lhset
+             ) {
+            noTekrar=noTekrar+w;
+        }
+        System.out.println(noTekrar);
+  }
 }
