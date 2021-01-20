c6c1da6f8b1f513910d8be9bbf4c24a6878c87a3
husamettinszn <husamettinszn@gmail.com>
2021-01-20 19:07:13 +0100

    ss

--- src/test/java/MentoringSoru.java
+++ src/test/java/MentoringSoru.java
@@ -1,5 +1,32 @@
 public class MentoringSoru {
     public static void main(String[] args) {
 
+        //String'den benzersiz deger nasil yazdirilir?
+
+        String  str = "tteetccehchppteerrocopt";
+
+        String tekrarsiz ="";
+
+        for (int i=0; i<str.length(); i++){
+            if (!tekrarsiz.contains(str.charAt(i)+"")){
+                tekrarsiz=tekrarsiz+str.charAt(i);
+            }
+        }
+        System.out.println(tekrarsiz);
+        System.out.println("====================");
+        //2_Yol
+        String noTekrar="";
+
+        String arr[] = str.split("");
+
+        for (String w:arr
+             ) {
+            if (!noTekrar.contains(w)) {
+                noTekrar=noTekrar+w;
+            }
+        }
+        System.out.println(noTekrar);
+
+
     }
 }
